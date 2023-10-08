49ade9bef8a1725f99c346f8d85c6843fde66292
cuter9 <cuter9@msn.com>
2023-10-08 11:31:12 +0800

    update

--- docker/display/enable.sh
+++ docker/display/enable.sh
@@ -4,4 +4,7 @@ sudo docker run -it -d \
     --network host \
     --privileged \
     --name=jetbot_display \
+    --env DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $HOME/.Xauthority:/root/.Xauthority \
     $JETBOT_DOCKER_REMOTE/jetbot:display-$JETBOT_VERSION-$L4T_VERSION
