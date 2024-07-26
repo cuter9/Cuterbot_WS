7988bc5b8c38ef59cc787a09f3af033e03b642c0
cuter <cuter9@msn.com>
2024-07-27 01:48:35 +0800

    Merge remote-tracking branch 'origin/master'

--- docker/display/enable.sh
+++ docker/display/enable.sh
@@ -7,4 +7,5 @@ sudo docker run -it -d \
     --env DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $HOME/.Xauthority:/root/.Xauthority \
+    -v /run/jtop.sock:/run/jtop.sock \
     $JETBOT_DOCKER_REMOTE/jetbot:display-$JETBOT_VERSION-$L4T_VERSION
--- jetbot_v1/yolo_tensorrt/__init__.py
+++
@@ -1,3 +0,0 @@
-# from .yolo_with_plugins_sync import *
-from .yolo_classes import *
-from .yolo_with_plugins import *
