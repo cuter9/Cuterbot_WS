18dce931abd3acd07c05af7823a7e3a39992da69
cuter <cuter9@msn.com>
2024-07-27 14:52:55 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ docker/display/enable_test_jtop.sh
@@ -0,0 +1,13 @@
+sudo docker run -it \
+    --restart always \
+    --runtime nvidia \
+    --network host \
+    --privileged \
+    --name=jetbot_display \
+    --env DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $HOME/.Xauthority:/root/.Xauthority \
+    -v /run/jtop.sock:/run/jtop.sock \
+    $JETBOT_DOCKER_REMOTE/jetbot:display-"$JETBOT_VERSION"-"$L4T_VERSION"
+    
+sudo systemctl restart jtop.service
