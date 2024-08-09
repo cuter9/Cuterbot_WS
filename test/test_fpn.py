f7d34e20ffd67e754ab1eaac478a2ca7246ec281
cuter <cuter9@msn.com>
2024-08-09 23:13:24 +0800

    Merge remote-tracking branch 'origin/master'

--- test/test_fpn.py
+++ test/test_fpn.py
@@ -9,7 +9,7 @@ WINDOW_NAME = 'TrtSsdModelTest'
 
 from jetbot import ObjectDetector
 
-follower_model = "/workspace/model_repo/object_detection/ssd_mobilenet_v2_fpnlite_320x320_coco17.engine"
+follower_model = "/home/cuterbot/model_repo/object_detection/ssd_mobilenet_v2_fpnlite_320x320_coco17.engine"
 
 detector = ObjectDetector()
 detector.engine_path = follower_model
