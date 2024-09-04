1f6f7df27b34969b60c2f95d8c5ddc0f29029ff7
cuter <cuter9@msn.com>
2024-09-04 12:47:38 +0800

    Merge remote-tracking branch 'origin/master'

--- docker/base/build_w_cv.sh
+++ docker/base/build_w_cv.sh
@@ -9,29 +9,30 @@ echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://ap
 sudo apt-get update
 export HOME=/home/cuterbot
 
-gdown --no-cookies --folder https://drive.google.com/drive/folders/1d1KtLKNBpOoTWb-Z1_pAaMtCIzfE6LyC -O ${HOME}/repo
-pushd ${HOME}/repo && unzip Video_Codec_SDK_12.1.14.zip && mv Video_Codec_SDK_12.1.14 Video_Codec_SDK
-pushd ./Video_Codec_SDK
-sudo cp ./Interface/*.h /usr/local/cuda-10.2/targets/aarch64-linux/include/
-sudo cp ./Lib/linux/stubs/aarch64/*.so /usr/local/cuda-10.2/targets/aarch64-linux/lib/
-
-if [ -f "/usr/local/cuda-10.2/targets/aarch64-linux/lib/libnvcuvid.so.1" ]; then
-  sudo rm "/usr/local/cuda-10.2/targets/aarch64-linux/lib/libnvcuvid.so.1"
+if [ ! -d "${HOME}/repo"]; then
+  gdown --no-cookies --folder https://drive.google.com/drive/folders/1d1KtLKNBpOoTWb-Z1_pAaMtCIzfE6LyC -O ${HOME}/repo
+  pushd ${HOME}/repo && unzip Video_Codec_SDK_12.1.14.zip && mv Video_Codec_SDK_12.1.14 Video_Codec_SDK
+  pushd ./Video_Codec_SDK
+  sudo cp ./Interface/*.h /usr/local/cuda-10.2/targets/aarch64-linux/include/
+  sudo cp ./Lib/linux/stubs/aarch64/*.so /usr/local/cuda-10.2/targets/aarch64-linux/lib/
+  
+  if [ -f "/usr/local/cuda-10.2/targets/aarch64-linux/lib/libnvcuvid.so.1" ]; then
+    sudo rm "/usr/local/cuda-10.2/targets/aarch64-linux/lib/libnvcuvid.so.1"
+  fi
+  sudo ln -s /usr/local/cuda-10.2/targets/aarch64-linux/lib/libnvcuvid.so /usr/local/cuda-10.2/targets/aarch64-linux/lib/libnvcuvid.so.1
+  
+  
+  if [ -f "/usr/local/cuda-10.2/targets/aarch64-linux/lib/libnvidia-encode.so.1" ]; then
+    sudo rm "/usr/local/cuda-10.2/targets/aarch64-linux/lib/libnvidia-encode.so.1"
+  fi
+  sudo ln -s /usr/local/cuda-10.2/targets/aarch64-linux/lib/libnvidia-encode.so /usr/local/cuda-10.2/targets/aarch64-linux/lib/libnvidia-encode.so.1
+  
+  popd
+  popd
 fi
-sudo ln -s /usr/local/cuda-10.2/targets/aarch64-linux/lib/libnvcuvid.so /usr/local/cuda-10.2/targets/aarch64-linux/lib/libnvcuvid.so.1
 
-
-if [ -f "/usr/local/cuda-10.2/targets/aarch64-linux/lib/libnvidia-encode.so.1" ]; then
-  sudo rm "/usr/local/cuda-10.2/targets/aarch64-linux/lib/libnvidia-encode.so.1"
-fi
-sudo ln -s /usr/local/cuda-10.2/targets/aarch64-linux/lib/libnvidia-encode.so /usr/local/cuda-10.2/targets/aarch64-linux/lib/libnvidia-encode.so.1
-
-popd
-popd
 docker build \
     --build-arg BASE_IMAGE="$JETBOT_BASE_IMAGE" \
-    --build-arg HOME=$HOME \
     -t "$JETBOT_DOCKER_REMOTE"/jetbot:base-"$JETBOT_VERSION"-$L4T_VERSION \
     -f Dockerfile_w_cv \
     $HOME 2>&1 | tee build_cuterbot_docker.log   # jetbot repo root as context
-
