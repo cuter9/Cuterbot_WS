710c2860bab11941ee841a52d97134e7e0896af4
cuter <cuter9@msn.com>
2024-09-23 02:02:12 +0800

    Merge remote-tracking branch 'origin/master'

--- test/test_yolo_v7.py
+++ test/test_yolo_v7.py
@@ -9,9 +9,9 @@ WINDOW_NAME = 'TrtSsdModelTest'
 
 from jetbot import ObjectDetector
 
-# follower_model = "/workspace/model_repo/object_detection/yolo_v7.engine"
+# follower_model = "/workspace/model_repo/object_detection/yolov7.engine"
 # follower_model = "/home/cuterbot/model_repo/object_detection/yolo_v7.engine"
-follower_model = "/home/cuterbot/model_repo/object_detection/yolo_v7-tiny.engine"
+follower_model = "/home/cuterbot/model_repo/object_detection/yolov7-tiny.engine"
 # follower_model = "/home/cuterbot/model_repo/object_detection/yolov4_416.engine"
 # follower_model = "/home/cuterbot/Data_Repo/Model_Conversion/SSD_mobilenet/ONNX_Model/Repo/ssd_mobilenet_v2_320x320_coco17_tpu-8_tf_v2.engine"
 
