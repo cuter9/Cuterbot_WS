1648996b24a2c803d00e0907ecdbc4c6c2676862
cuter <cuter9@msn.com>
2024-10-19 09:42:31 +0800

    Merge remote-tracking branch 'origin/master'

--- jetbot/utils/model_selection_0.py
+++ jetbot/utils/model_selection_0.py
@@ -14,83 +14,40 @@ os.environ['MODEL_REPO_DIR'] = MODEL_REPO_DIR
 
 
 def load_tune_pth_model(pth_model_name="resnet18", pretrained=True):
-    """
     if pretrained:
         model = getattr(pth_models, pth_model_name)()  # for fine-tuning
     else:
         model = getattr(pth_models, pth_model_name)(pretrained=False)  # for inferencing
-    """
-    model_type = None
-    model = None
+
 # ----- modify the last layer for classification, and the model used in notebook should be modified too.
     if 'mobilenet_v3' in pth_model_name: # 'mobilenet_v3_large' or  'mobilenet_v3_small'
         model_type = "MobileNet"
-        weights_cls = None
-        if pretrained:
-            if "small" in pth_model_name:
-                weights_cls = "MobileNet_V3_Small_Weights"
-            elif "large" in pth_model_name:
-                weights_cls = "MobileNet_V3_Large_Weights"
-            else:
-                assert weights_cls is not None, "Check the use of the pretrained model!"
-            weights = getattr(pth_models, weights_cls).DEFAULT
-            model = getattr(pth_models, pth_model_name)(weights=weights)  # for fine-tuning
-        else:
-            model = getattr(pth_models, pth_model_name) # for inferencing
         model.classifier[3] = torch.nn.Linear(model.classifier[3].in_features,
                                               2)  # for mobilenet_v3 model. must add the block expansion factor 4
 
     elif pth_model_name == 'mobilenet_v2':
         model_type = "MobileNet"
-        if pretrained:
-            weights = getattr(pth_models, "MobileNet_V2_Weights").DEFAULT
-            model = getattr(pth_models, pth_model_name)(weights=weights)  # for fine-tuning
-        else:
-            model = getattr(pth_models, pth_model_name) # for inferencing
         model.classifier[1] = torch.nn.Linear(model.classifier[1].in_features,
                                               2)  # for mobilenet_v2 model. must add the block expansion factor 4
 
     elif pth_model_name == 'vgg11':  # VGGNet
         model_type = "VggNet"
-        if pretrained:
-            weights = getattr(pth_models, "VGG11_Weights").DEFAULT
-            model = getattr(pth_models, pth_model_name)(weights=weights)  # for fine-tuning
-        else:
-            model = getattr(pth_models, pth_model_name) # for inferencing
         model.classifier[6] = torch.nn.Linear(model.classifier[6].in_features,
                                               2)  # for VGG model. must add the block expansion factor 4
 
     elif 'resnet' in pth_model_name:  # ResNet
         model_type = "ResNet"
-        if pretrained:
-            weights_cls = pth_model_name.replace("resnet", "ResNet")+"_Weights"
-            weights = getattr(pth_models, weights_cls).DEFAULT
-            model = getattr(pth_models, pth_model_name)(weights=weights)  # for fine-tuning
-        else:
-            model = getattr(pth_models, pth_model_name)  # for inferencing
         model.fc = torch.nn.Linear(model.fc.in_features,
                                    2)  # for resnet model must add the block expansion factor 4
         # model.fc = torch.nn.Linear(512, 2)
 
     elif 'efficientnet' in pth_model_name:  # ResNet
         model_type = "EfficientNet"
-        if pretrained:
-            weights_cls = pth_model_name.replace("efficientnet_b", "EfficientNet_B")+"_Weights"
-            weights = getattr(pth_models, weights_cls).DEFAULT
-            model = getattr(pth_models, pth_model_name)(weights=weights)  # for fine-tuning
-        else:
-            model = getattr(pth_models, pth_model_name)  # for inferencing
-
         model.classifier[1] = torch.nn.Linear(model.classifier[1].in_features, 2)  # for efficientnet model
         # model.classifier[0].dropout = torch.nn.Dropout(p=dropout)
 
     elif pth_model_name == 'inception_v3':  # Inception_v3
         model_type = "InceptionNet"
-        if pretrained:
-            weights = getattr(pth_models, "Inception_V3_Weights").DEFAULT
-            model = getattr(pth_models, pth_model_name)(weights=weights)  # for fine-tuning
-        else:
-            model = getattr(pth_models, pth_model_name) # for inferencing
         model.fc = torch.nn.Linear(model.fc.in_features, 2)
         # model.dropout = torch.nn.Dropout(p=dropout)
         if model.aux_logits:
@@ -98,11 +55,6 @@ def load_tune_pth_model(pth_model_name="resnet18", pretrained=True):
 
     elif pth_model_name == 'googlenet':  # Inception_v3
         model_type = "GoogleNet"
-        if pretrained:
-            weights = getattr(pth_models, "GoogLeNet_Weights").DEFAULT
-            model = getattr(pth_models, pth_model_name)(weights=weights)  # for fine-tuning
-        else:
-            model = getattr(pth_models, pth_model_name) # for inferencing
         model.fc = torch.nn.Linear(model.fc.in_features, 2)
         # model.dropout = torch.nn.Dropout(p=dropout)
         if model.aux_logits:
@@ -113,36 +65,18 @@ def load_tune_pth_model(pth_model_name="resnet18", pretrained=True):
 
     elif "densenet" in pth_model_name:  # densenet121, densenet161, densenet169, densenet201
         model_type = "DenseNet"
-        if pretrained:
-            weights_cls = pth_model_name.replace("densenet", "DenseNet")+"_Weights"
-            weights = getattr(pth_models, weights_cls).DEFAULT
-            model = getattr(pth_models, pth_model_name)(weights=weights)  # for fine-tuning
-        else:
-            model = getattr(pth_models, pth_model_name)  # for inferencing
         model.classifier = torch.nn.Linear(model.classifier.in_features, 2)
 
     elif "shufflenet_v2" in pth_model_name:  # shufflenet_v2_x1_0 or shufflenet_v2_x0_5
         model_type = "ShuffleNet"
-        if pretrained:
-            weights_cls = pth_model_name.replace("shufflenet_v2_x", "ShuffleNet_V2_X")+"_Weights"
-            weights = getattr(pth_models, weights_cls).DEFAULT
-            model = getattr(pth_models, pth_model_name)(weights=weights)  # for fine-tuning
-        else:
-            model = getattr(pth_models, pth_model_name)  # for inferencing
         model.fc = torch.nn.Linear(model.fc.in_features, 2)
 
     elif "mnasnet" in pth_model_name:  # mnasnet1_0 or mnasnet0_5
         model_type = "MnasNet"
-        if pretrained:
-            weights_cls = pth_model_name.replace("mnasnet", "MNASNet")+"_Weights"
-            weights = getattr(pth_models, weights_cls).DEFAULT
-            model = getattr(pth_models, pth_model_name)(weights=weights)  # for fine-tuning
-        else:
-            model = getattr(pth_models, pth_model_name)  # for inferencing
         model.classifier[1] = torch.nn.Linear(model.classifier[1].in_features, 2)
 
     else:
-        assert (model is not None and model_type is not None), "Check if the model name set is compatible with torchvision."
+        model_type = None
 
     return model, model_type
 
